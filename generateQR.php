08a10137548cb633deb2ed11aca7e2cea992e0b6
Esha Ranalkar <esharanalkar7@gmail.com>
2019-03-10 18:05:24 +0000

    Initial commit

---
+++ generateQR.php
@@ -0,0 +1,55 @@
+<?php
+// include QR_BarCode class
+
+include "QR_BarCode.php";
+
+//// Selecting from Database
+include_once 'config.php';
+$sql = 'SELECT * FROM user_profile WHERE username=username';
+foreach ($connect->query($sql) as $row)
+
+if(isset($errMsg)){
+    echo '<div style="color:green;text-align:center;font-size:17px;">'.$errMsg.'</div>';
+}
+
+
+$tempDir = 'temp/';
+$firstname = $row['firstname'];
+$lastname = $row['lastname'];
+$email = $row['email'];
+$jobtitle = $row['jobtitle'];
+$company = $row['company'];
+$job_desc = $row['job_desc'];
+$telephone = $row['telephone'];
+$linkedin = $row['linkedin'];
+$twitter = $row['twitter'];
+$instagram = $row['instagram'];
+$facebook= $row['facebook'];
+
+// QR_BarCode object
+$qr = new QR_BarCode();
+
+
+// create content QR code
+$qr->content("$firstname",
+    "$lastname",
+    "$email",
+    "$jobtitle",
+    "$company",
+    "$job_desc",
+    "$telephone",
+    "$linkedin",
+    "$twitter",
+    "$instagram",
+    "$facebook" );
+
+// display QR code image
+$qr->qrCode();
+
+// save QR code image
+
+/// Should save a new image for new user >>>> need to check later
+$qr->qrCode(500,'img/cw-qr.png');
+
+header('Location: viewcard.php');
+?>
