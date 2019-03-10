bca6ef25b36449a371f998c8acae6b00638de079
Esha Ranalkar <esharanalkar7@gmail.com>
2019-03-10 20:01:24 +0000

    Added the session profile to view details of the logged in user in update_profile1, viewcard and generateQR changed header on viewcard

--- viewcard.php
+++ viewcard.php
@@ -63,7 +63,7 @@ $facebook= $row['facebook'];
         <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="active navbar-brand" href="#">VIVA CARD</a>
+                    <a class="active navbar-brand" href="index.php">VIVA CARD</a>
                 </div>
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="dashboard.php">Home</a></li>
